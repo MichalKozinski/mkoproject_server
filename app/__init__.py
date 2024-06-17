2276f37448e91a34933d2bc503a442995210701a
MichalKozinski <knf@vp.pl>
2024-06-17 09:46:04 +0000

    made the structure of the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from app import api  # Importowanie widoków
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-from app import api  # Importowanie widoków
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from .views import views
+from .auth import auth
+
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
+
+from .models import get_user_by_id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return get_user_by_id(user_id)
